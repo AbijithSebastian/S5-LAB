383b436b9e73b92c3341427db75b486d16e9cd9e
AbijithSebastian <abijithseban14@gmail.com>
2024-11-19 12:18:35 +0530

    Add files via upload

---
+++ OS programs/CPU scheduling/fcfs.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+struct  process
+{
+    int id,at,bt,ct,tat,wt;
+}p[30],temp;
+
+void ganttchart(int num)
+{
+    for(int i=1;i<=2*p[num].ct-4;i++)
+    {
+        printf("-");
+    }
+    printf("\n");
+    for(int i=1;i<=num;i++)
+    {
+        printf("|");
+        for(int j=1;j<=p[i].bt/2;j++)
+        {
+            printf(" ");
+        }
+        printf("P%d",p[i].id);
+        for(int j=1;j<=p[i].bt/2;j++)
+        {
+            printf(" ");
+        }
+    }
+    printf("|\n");
+    for(int i=1;i<=2*p[num].ct-4;i++)
+    {
+        printf("-");
+    }
+
+    printf("\n0");
+    for(int i=1;i<=num;i++)
+    {
+        for(int j=1;j<=p[i].bt/2;j++)
+        {
+            printf(" ");
+        }
+        if(p[i].ct<=9)
+        {
+            printf("  ");
+        }
+        else
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=p[i].bt/2;j++)
+        {
+            printf(" ");
+        }
+        printf("%d",p[i].ct);
+    }
+
+}
+
+void main()
+{
+    int num,sumtat=0,sumwt=0;
+    printf("Enter the no.of processes:");
+    scanf("%d",&num);
+    printf("Enter the processes:\n");
+    for(int i=1;i<=num;i++)
+    {
+        p[i].id=i;
+        printf("Enter the AT of P%d:",i);
+        scanf("%d",&p[i].at);
+        printf("Enter the BT of P%d:",i);
+        scanf("%d",&p[i].bt);
+    }
+    p[1].ct=p[1].bt;
+    for(int i=1;i<=num;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<=num;j++)
+        {
+            if(p[j].at<=p[i-1].ct  && p[j].at<p[min].at)
+            {
+                min=j;
+            }
+        }
+        temp=p[i];
+        p[i]=p[min];
+        p[min]=temp;
+        p[i].ct=p[i-1].ct+p[i].bt;
+        if(p[i].at>p[i-1].ct)
+        {
+            printf("idle time fron %d to %d\n",p[i-1].ct,p[i].at);
+            p[i].ct+=p[i].at-p[i-1].ct;
+        }
+    }
+
+    for(int i=1;i<=num;i++)
+    {
+        p[i].tat=p[i].ct-p[i].at;
+        p[i].wt=p[i].tat-p[i].bt;
+        sumtat+=p[i].tat;
+        sumwt+=p[i].wt;
+    }
+    printf("Average TAT:%.2f\nAverage WT:%.2f\n",(float)sumtat/num,(float)sumwt/num);
+
+    printf("PID\tAT\tBT\tCT\tTAT\tWT\n");
+    for(int i=1;i<=num;i++)
+    {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",p[i].id,p[i].at,p[i].bt,p[i].ct,p[i].tat,p[i].wt);
+    }
+    ganttchart(num);
+}
